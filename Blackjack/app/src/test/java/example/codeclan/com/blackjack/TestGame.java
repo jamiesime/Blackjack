56603a2687fc7c60ec4ce9659f948d43198c57a4
Jamie Sime <jamiesime@gmail.com>
2017-10-31 14:54:21 +0000

    added tests to check some dealer logic

--- Blackjack/app/src/test/java/example/codeclan/com/blackjack/TestGame.java
+++ Blackjack/app/src/test/java/example/codeclan/com/blackjack/TestGame.java
@@ -1,8 +1,47 @@
 package example.codeclan.com.blackjack;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 31/10/2017.
  */
 
 public class TestGame {
+
+    Dealer dealer;
+    Player player;
+    Game game;
+    Deck deck;
+    Card card;
+
+    @Before
+    public void setUp() {
+        deck = new Deck();
+        dealer = new Dealer("Gregario", deck);
+        player = new Player("City Steve");
+        game = new Game(player, dealer, deck);
+    }
+
+
+
+    @Test
+    public void TestDealerHasAce(){
+        dealer.cards.add(new Card(Suit.CLUBS, Rank.ACE));
+        dealer.cards.add(new Card(Suit.CLUBS, Rank.ACE));
+        dealer.cards.add(new Card(Suit.CLUBS, Rank.THREE));
+        assertEquals(true, game.checkForAce(dealer));
+    }
+
+    @Test
+    public void TestDealerMakesDecision(){
+        dealer.cards.add(new Card(Suit.CLUBS, Rank.ACE));
+        dealer.cards.add(new Card(Suit.CLUBS, Rank.ACE));
+        dealer.cards.add(new Card(Suit.CLUBS, Rank.THREE));
+        int choice = dealer.decision(true);
+        assertEquals(2, choice);
+    }
+
 }
